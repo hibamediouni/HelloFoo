26f0dba3e1a58a7619faaa9109b36c44a3729150
Hiba mediouni <hmediouni@cs.experts-comptables.org>
2020-01-29 14:35:50 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ import { Component } from '@angular/core';
         <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
       </li>
     </ul>
-    
+    <lib-ComponentsLibrary></lib-ComponentsLibrary>
   `,
   styles: []
 })
